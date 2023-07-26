cc7ca426ce598abf257ee90f0081b15da968899c
electimon <electimon@gmail.com>
2023-07-26 20:22:15 +0800

    gpu_mali: Update from tesla tag
    Signed-off-by: electimon <electimon@gmail.com>

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
@@ -6,8 +6,6 @@
 #ifndef __MTK_PLATFORM_DEBUG_H__
 #define __MTK_PLATFORM_DEBUG_H__
 
-#include <mali_kbase_defs.h>
-
 void mtk_common_debug_dump_status(void);
 #if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
 int mtk_common_debug_init(void);
@@ -21,5 +19,6 @@ void mtk_common_debug_logbuf_dump(struct mtk_debug_logbuf *logbuf, struct seq_fi
 void mtk_common_gpu_fence_debug_dump(int fd, int pid, int type, int timeouts);
 
 extern void (*mtk_gpu_fence_debug_dump_fp)(int fd, int pid, int type, int timeouts);
+void mtk_debug_csf_dump_groups_and_queues(struct kbase_device *kbdev, int pid);
 
 #endif /* __MTK_PLATFORM_DEBUG_H__ */
