b70e305be375bc101ff515c8c9479c7e14710b1d
electimon <electimon@gmail.com>
2024-05-22 02:38:51 +0000

    drivers: gpu/mediatek: Fix building
    Signed-off-by: electimon <electimon@gmail.com>

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/base/arm/protected_memory_allocator/protected_memory_allocator.c
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/base/arm/protected_memory_allocator/protected_memory_allocator.c
@@ -27,7 +27,7 @@
 #include <linux/slab.h>
 #include <linux/mm.h>
 #include <linux/io.h>
-#include <linux/protected_memory_allocator.h>
+#include "../../../gpu/arm/midgard/include/linux/protected_memory_allocator.h"
 
 /* Size of a bitfield element in bytes */
 #define BITFIELD_ELEM_SIZE sizeof(u64)
