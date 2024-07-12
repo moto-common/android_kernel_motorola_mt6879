914133cf75ea2894c02bd9a248b8c776c26a68d6
electimon <electimon@gmail.com>
2024-07-12 01:58:03 +0000

    treewide: Remove useless logging
    Signed-off-by: electimon <electimon@gmail.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
@@ -4189,7 +4189,6 @@ void mtk_drm_top_clk_disable_unprepare(struct drm_device *drm)
 		clk_disable_unprepare(priv->top_clk[i]);
 	}
 
-	DDPMSG("%s: mtcmos off\n", __func__);
 	pm_runtime_put_sync(priv->mmsys_dev);
 	if (priv->side_mmsys_dev)
 		pm_runtime_put_sync(priv->side_mmsys_dev);
